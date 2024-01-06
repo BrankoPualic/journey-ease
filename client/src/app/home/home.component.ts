9c13ef00a0ee7d401634987e235b3033d82ea076
Bane1110 <brankaccc@gmail.com>
2024-01-06 15:57:41 +0100

    Header and Cover

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SearchHomeComponent } from './search-home/search-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SearchHomeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
