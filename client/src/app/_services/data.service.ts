c13a581c93da4eb4d289ca2284e6786c41610a14
Bane1110 <brankaccc@gmail.com>
2024-01-06 18:36:00 +0100

    Cover done, Added dynamic input fields, services, types and environment

---
+++ client/src/app/_services/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  get<T>(url: string) {
+    return this.http.get<T>(this.baseUrl + url);
+  }
+}
