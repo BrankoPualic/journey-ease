b19756116e4388e9522d3448226e3fa49e4a32c4
Bane1110 <brankaccc@gmail.com>
2024-01-18 20:13:30 +0100

    Error handling

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,17 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { countryReducer } from './_store/countries/countries.reducer';
 import { CountryEffects } from './_store/countries/countries.effects';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     provideStore({ countries: countryReducer }),
     provideEffects([CountryEffects]),
   ],
